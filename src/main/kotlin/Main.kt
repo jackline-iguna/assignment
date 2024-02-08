7cb4314e71c61c75a4c7ee319a579f9bef2c7015
jackline <jacklineiguna@gmail.com>
2024-02-08 06:50:43 +0300

    variable

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main( ){
+    var studentsNames = "Jackline"
+    println(jackline)
+    var age = 20
+    println(20)
+    var phoneNumber = "0146785765"
+    printin (0146785765)
+    var weight ='66'
+    pritin (65)
+    var nationality = "kenyan"
+    pritin (kenyan)
+    val citizen =true
+    val ninCitizen = false
+
+
+
+
+
+
+}
